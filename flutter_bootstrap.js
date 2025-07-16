12476f3c806ec170cf476902740ff696783a0c59
PavMash <PavMash@users.noreply.github.com>
2025-07-16 22:58:53 +0000

    deploy: 7fab04ff75241bc0e37f4ac9ce4d110003346e0f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4067940732"
+    serviceWorkerVersion: "144618094"
   }
 });
